4601d8c96ac21683ac40093eb8e43d062e2cd294
Austin Aspaas <araspaas@live.com>
2020-12-01 14:56:43 -0700

    Add tests for having a log in form on the welcome page as well as a link to register a new account

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-describe "as a visitor" do
-  describe "when i visit the home page" do
+describe "As a visitor" do
+  describe "When i visit the home page" do
     it "I see welcome text and a shor description of the site" do
 
       visit root_path
@@ -10,8 +10,18 @@ describe "as a visitor" do
       expect(page).to have_content("This website allows you to create a 'viewing party' with your friends to watch movies together online from the comfort of your own homes. Especially during these hard time during a pandemic when it isn't safe to be in the same room.")
     end
 
-    it "text" do
+    it "I see a button to log in along with email and password form fields" do
+      visit root_path
 
+      expect(page).to have_field("email")
+      expect(page).to have_field("password")
+      expect(page).to have_button("Log In")
+    end
+
+    it "I see a link to register to viewing party" do
+      visit root_path
+
+      expect(page).to have_link("New to Viewing Party? Register Here")
     end
   end
 end
