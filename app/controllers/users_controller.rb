40db147105596363f91dc3e3ee5cab8c77aec511
Kiera Allen <kieraaallen@gmail.com>
2020-12-02 16:56:18 -0700

    Add create action in users controller
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+# binding.pry
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = "You are now registered and logged in."
+      session[:user_id] = user.id
+      redirect_to dashboard_index_path
+    else
+      flash[:error] = user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
 end
