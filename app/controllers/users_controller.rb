c19915db47363be1da59d93831fb76814e344bec
Kiera Allen <kieraaallen@gmail.com>
2020-12-03 13:51:47 -0700

    Remove comments that showed previous code
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    # params.permit(:name, :email, :password)
     params.permit(:name, :email, :password, :password_confirmation)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   validates_presence_of :name,
                         :email
   validates_presence_of :password, if: :password
-  # validates :password, confirmation: true, presence: true
   validates :email, uniqueness: true
 end
