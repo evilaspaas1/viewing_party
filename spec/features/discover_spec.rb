632c37e8e7f30da211f2696203873db55956b1a4
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-03 16:08:46 -0700

    Update spec with correct RSpec syntax
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- spec/features/discover_spec.rb
+++ spec/features/discover_spec.rb
@@ -15,32 +15,32 @@ describe 'as a user on the discover page' do
   end
 
   it 'has a search field to search by movie title and button to initiate' do
-    expect(page).to have_field(:search_by_title)
-    expect(page).to have_button("Find Movies")
+    expect(page).to have_field(:search)
+    expect(page).to have_button("Search By Title")
   end
 
   it 'when the button to find movies is clicked it takes us to the movies page' do
-    fill_in(:search_by_title).with("Titanic")
-    click_button("Find Movies")
+    fill_in :search, with: "Titanic"
+    click_button("Search By Title")
     expect(current_path).to eq("/movies")
   end
 
   it 'returns an error if the field is not filled out' do
-    fill_in(:search_by_title).with("")
-    click_button("Find Movies")
+    fill_in :search, with: ""
+    click_button("Search By Title")
     expect(current_path).to eq("/discover")
     expect(page).to have_content("Must provide a search query")
   end
 
   it 'can search for movies in any case' do
-    fill_in(:search_by_title).with("tiTanIc")
-    click_button("Find Movies")
+    fill_in :search, with: "tiTanIc"
+    click_button("Search By Title")
     expect(current_path).to eq("/movies")
   end
 
   it 'can search for partial keyword/movie title' do
-    fill_in(:search_by_title).with("phoe")
-    click_button("Find Movies")
+    fill_in :search, with: "phoe"
+    click_button("Search By Title")
     expect(current_path).to eq("/movies")
   end
 end
