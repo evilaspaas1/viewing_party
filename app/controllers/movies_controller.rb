ed09bc7724c3666c4db5426a425e339a4fb77a87
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-04 19:07:47 -0700

    Update movies controller with movies API calls in index action
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,20 @@ class MoviesController < ApplicationController
     if params[:search] == ""
       flash[:error] = "Must provide a search query"
       redirect_to "/discover"
-    end 
+    elsif params[:search]
+      # search_term = params[:search]
+      # require "pry"; binding.pry
+      movies_by_search = Faraday.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&query=#{params[:search]}")
+      json = JSON.parse(movies_by_search.body, symbolize_names: :true)
+      @movies = json[:results]
+      # require "pry"; binding.pry
+    else
+      top_20 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&page=1")
+      second_20 = Faraday.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&page=2")
+      parsed_top_20 = JSON.parse(top_20.body, symbolize_names: :true)
+      parsed_second_20 = JSON.parse(second_20.body, symbolize_names: :true)
+      @movies = (parsed_top_20[:results] << parsed_second_20[:results]).flatten
+      # require "pry"; binding.pry
+    end
   end
 end
