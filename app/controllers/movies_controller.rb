a3cb104b2a729470feced81ff5e9541e72042f59
Austin Aspaas <araspaas@live.com>
2020-12-05 14:56:36 -0700

    Add show action and API calls to movies controller
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,4 +15,18 @@ class MoviesController < ApplicationController
       @movies = (parsed_top_20[:results] << parsed_second_20[:results]).flatten
     end
   end
+
+  def show
+    movie_details = Faraday.get("https://api.themoviedb.org/3/movie/#{params[:id]}?api_key=#{ENV['MOVIES_API_KEY']}")
+    @movie = JSON.parse(movie_details.body, symbolize_names: :true)
+
+    cast_data = Faraday.get("https://api.themoviedb.org/3/movie/#{params[:id]}/credits?api_key=#{ENV['MOVIES_API_KEY']}")
+    cast_json = JSON.parse(cast_data.body, symbolize_names: :true)
+    @cast = cast_json[:cast].first(10)
+
+    review_data = Faraday.get("https://api.themoviedb.org/3/movie/#{params[:id]}/reviews?api_key=#{ENV['MOVIES_API_KEY']}")
+    review_json = JSON.parse(review_data.body, symbolize_names: :true)
+    @total_results = review_json[:total_results]
+    @reviews = review_json[:results]
+  end
 end
