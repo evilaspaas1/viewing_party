b381aae6fb463739b4d1e0bd7492331d1a8e1961
Kiera Allen <kieraaallen@gmail.com>
2020-12-05 17:56:16 -0700

    Create test for dashboard friendships section
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -4,6 +4,10 @@ describe 'As a registered user' do
   describe 'when I am on my dashboard' do
     before :each do
       @user = User.create!(name: "Tim", email: "tim@gmail.com", password: "test")
+      @shaunda = User.create!(name: "Shaunda", email: "shaunda@gmail.com", password: "test")
+      @austin = User.create!(name: "Austin", email: "austin@gmail.com", password: "test")
+      @kiera = User.create!(name: "Kiera", email: "kiera@gmail.com", password: "test")
+
       visit root_path
       fill_in :email, with: @user.email
       fill_in :password, with: @user.password
@@ -26,6 +30,51 @@ describe 'As a registered user' do
 
     it "has a viewing parties section" do
       expect(page).to have_css("section.viewing_parties")
+    end
+
+    it "has a text field to enter a friend's email and a button to addasd that friend" do
+      within("section.friends") do
+        expect(page).to have_field(:email)
+        expect(page).to have_button("Add Friend")
+      end
+    end
+
+    it "shows a 'You Currently Have No Friends' message if I haven't added any friends" do
+      within("section.friends") do
+        expect(page).to have_content("You Currently Have No Friends")
+      end
+    end
+
+    it "I can add a friend using their email as long as they exist in database" do
+
+      within("section.friends") do
+        fill_in :email, with: @shaunda.email
+        click_button "Add Friend"
+        expect(current_path).to eq("/dashboard")
+
+        fill_in :email, with: "today@gmail.com"
+        click_button "Add Friend"
+
+        expect(current_path).to eq("/dashboard")
+      end
+      expect(page).to have_content("I'm sorry your friend cannot be found")
+    end
+
+    it "shows a list of all my friends that I have added" do
+      fill_in :email, with: @shaunda.email
+      click_button "Add Friend"
+
+      fill_in :email, with: @austin.email
+      click_button "Add Friend"
+
+      fill_in :email, with: @kiera.email
+      click_button "Add Friend"
+
+      within("section.friends") do
+        expect(page).to have_content(@shaunda.name)
+        expect(page).to have_content(@austin.name)
+        expect(page).to have_content(@kiera.name)
+      end
     end
   end
 end
