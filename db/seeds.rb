5e6f6bd006ff7435f024591eb09fd223327f36be
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-08 13:19:33 -0700

    Add seeds to test database
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Movie.destroy_all
+Party.destroy_all
+Guest.destroy_all
+
 user = User.create!(name: "Tim", email: "tim@gmail.com", password: "test")
+harry_potter = Movie.create!({title: "Harry Potter and the Philosopher's Stone", duration: 152, api_id: 671})
+hp_watch_party = user.parties.create!({movie_id: harry_potter.id, date: DateTime.now.to_date.to_s, start_time: DateTime.now.to_time.to_s[11..15], duration: 152 })
+guest = user.guests.create!({party_id: hp_watch_party.id})
