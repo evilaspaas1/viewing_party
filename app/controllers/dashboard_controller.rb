4a7cc5de04e03caf77ec2498f3feafadc53f6b28
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-08 13:22:19 -0700

    Update index action with finding parties associated with current user
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,17 @@
 class DashboardController < ApplicationController
   before_action :require_user
+
   def index
+    # require "pry"; binding.pry
+    guests = @current_user.guests
+    @parties = guests.map do |guest|
+      Party.find_by(id: guest.party_id)
+    end
+    # require "pry"; binding.pry
   end
 
-
   private
+
   def require_user
     render file: "public/404" unless current_user
   end
