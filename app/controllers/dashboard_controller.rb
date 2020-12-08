2a7c954d726ba84fea7ffa617847f2e49384c9bf
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-08 13:53:47 -0700

    Removie prys

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,10 @@ class DashboardController < ApplicationController
   before_action :require_user
 
   def index
-    # require "pry"; binding.pry
     guests = @current_user.guests
     @parties = guests.map do |guest|
       Party.find_by(id: guest.party_id)
     end
-    # require "pry"; binding.pry
   end
 
   private
