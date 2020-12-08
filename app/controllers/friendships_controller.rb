76850cbe001e378bef6186675f43d9c83b773c45
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-08 13:43:00 -0700

    Update friendship create action with further validations
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,27 @@
 class FriendshipsController < ApplicationController
 
   def create
-    friend = User.find_by(email: params[:email])
-    if friend
-      Friendship.create({user_id: current_user.id, friend_id: friend.id})
-    else
-      flash[:errors] = "I'm sorry your friend cannot be found"
-    end
+    new_friend = User.find_by(email: params[:email])
 
+    create_friendship(current_user.id, new_friend)
     redirect_to "/dashboard"
+  end
+
+  def create_friendship(user_id, friend)
+    if !friend
+      flash[:error] = "I'm sorry your friend cannot be found"
+    elsif friend.id == user_id
+      flash[:error] = "You can not add yourself as a friend"
+    elsif friend_exists?(friend)
+      flash[:error] = "You have already added this friend"
+    else friend
+      Friendship.create({user_id: current_user.id, friend_id: friend.id})
+    end
+  end
+
+  def friend_exists?(new_friend)
+    current_user.friends.any? do |friend|
+      friend.id == new_friend.id
+    end
   end
 end
