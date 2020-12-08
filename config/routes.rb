4522522672cf535bb5fbd5ecba0204a7961d5e6e
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-08 10:52:35 -0700

    Update routes to reflect name change to party
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   resources :movies, only: [:index, :show]
 
-  resources :viewing_party, only: [:new, :create]
+  resources :party, only: [:new, :create]
 
   resources :friendships, only: [:create]
 end
