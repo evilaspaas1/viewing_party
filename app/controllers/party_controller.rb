ffc2a4143c19bbe6b59fcf3baf4ee5724fe80818
Austin Aspaas <araspaas@live.com>
2020-12-08 15:03:02 -0700

    Add logic that does not allow creation of same movie twice in the database.
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -1,7 +1,12 @@
 class PartyController < ApplicationController
 
   def new
-
+    @movie = Movie.find_by(api_id: params[:api_id])
+    if @movie
+      @movie
+    else
+      Movie.create!({title: params[:title], duration: params[:duration], api_id: params[:api_id]})
+    end
   end
 
   # def create
