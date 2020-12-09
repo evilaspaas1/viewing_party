54e37dc44885dbf07ee08ab7f3065df05c6af0fb
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-09 15:06:55 -0700

    Create new POROs for actor and review
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

---
+++ app/poros/review.rb
@@ -0,0 +1,8 @@
+class Review
+  attr_reader :username,
+              :content
+  def initialize(attributes)
+    @username = attributes[:author_details][:username]
+    @content = attributes[:content]
+  end
+end
