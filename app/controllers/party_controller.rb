d3cd90777eadf8bf736b05ff6755973f56254f60
Austin Aspaas <araspaas@live.com>
2020-12-08 19:17:48 -0700

    Add create action in party controller and party params method
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -5,11 +5,32 @@ class PartyController < ApplicationController
     if @movie
       @movie
     else
-      Movie.create!({title: params[:title], duration: params[:duration], api_id: params[:api_id]})
+      @movie = Movie.create!({title: params[:title], duration: params[:duration], api_id: params[:api_id]})
     end
   end
 
-  # def create
-  #
-  # end
+  def create
+    party = Party.new(party_params)
+    if party.save && params[:friends]
+      Guest.create!(party_id: party.id, user_id: current_user.id)
+      params[:friends][:ids].each do |id|
+        unless id == ""
+          Guest.create!(party_id: party.id, user_id: id)
+        end
+      end
+      redirect_to dashboard_index_path
+    elsif party.save
+      Guest.create!(party_id: party.id, user_id: current_user.id)
+      redirect_to dashboard_index_path
+    else
+      flash[:notice] = party.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+
+  private
+  def party_params
+    params.permit(:date, :duration, :start_time, :movie_id, :user_id)
+  end
 end
