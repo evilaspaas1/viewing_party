ef65ae7681dd55d3534ff140995048c489b3b7a4
Austin Aspaas <araspaas@live.com>
2020-12-09 18:20:04 -0700

    Add tests for the movie facade
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>

---
+++ spec/facades/movie_facade_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+describe "Movie Facade" do
+  it "returns a list of searched movies objects" do
+    VCR.use_cassette('movies_by_search') do
+      movies = MovieFacade.movies_by_search("Phoenix")
+
+      expect(movies).to be_an(Array)
+      expect(movies.size).to eq(20)
+      expect(movies.first).to be_an_instance_of(MovieDetails)
+    end
+  end
+
+  it "returns top movie objects" do
+    VCR.use_cassette('top_movies') do
+      movies = MovieFacade.top_40_movies
+
+      expect(movies).to be_an(Array)
+      expect(movies.size).to eq(40)
+      expect(movies.first).to be_an_instance_of(MovieDetails)
+    end
+  end
+
+  it "returns movie object" do
+    VCR.use_cassette("movie_details2") do
+      movie = MovieFacade.movie_data(278)
+
+      expect(movie).to be_an_instance_of(MovieDetails)
+    end
+  end
+
+  it "returns an actor object" do
+    VCR.use_cassette('movie_details2') do
+      actors = MovieFacade.cast_data(278)
+
+      expect(actors).to be_an(Array)
+      expect(actors.size).to eq(10)
+      expect(actors.first).to be_an_instance_of(Actor)
+    end
+  end
+
+  it "returns a review object" do
+    VCR.use_cassette('movie_details2') do
+      review = MovieFacade.review_data(278)
+
+      expect(review).to be_an(Array)
+      expect(review.count).to eq(6)
+      expect(review.first).to be_an_instance_of(Review)
+    end
+  end
+end
