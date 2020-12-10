5ce706168a285356690267da6606c9fa6ed89900
Austin Aspaas <araspaas@live.com>
2020-12-09 18:18:40 -0700

    Remove redundant tests and add sad path test for creating a bad party
    Co-authored-by: Kiera Allen <46658858+kieraallen@users.noreply.github.com>
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -39,20 +39,6 @@ describe "As a registered user" do
       end
     end
 
-    xit "party duration can't be less that movie duration" do
-      movie = Movie.last
-      within ".party_form" do
-        fill_in :duration, with: 1
-        click_button "Create Viewing Party"
-      end
-
-      expect(page).to have_content("Party duration must be longer or same as movie duration")
-
-      within ".party_form" do
-        expect(page).to have_field(:duration, with: movie.duration)
-      end
-    end
-
     it "can choose friend(s) to attend by checking boxes" do
       within ".party_form" do
         check "#{@shaunda.name}"
@@ -67,18 +53,6 @@ describe "As a registered user" do
       end
     end
 
-    it "redirect me to the user dashboard when i click create party button" do
-      within ".party_form" do
-        fill_in :duration, with: 1
-        fill_in :date, with: "12-08-2020"
-        fill_in :start_time, with: "7:00"
-        check "#{@shaunda.name}"
-        check "#{@kiera.name}"
-        click_button "Create Viewing Party"
-      end
-      expect(current_path).to eq(dashboard_index_path)
-    end
-
     it "I can see the new party on my dashboard page" do
       movie = Movie.last
       within ".party_form" do
@@ -91,6 +65,29 @@ describe "As a registered user" do
       end
       expect(current_path).to eq(dashboard_index_path)
       expect(page).to have_content("2020-08-12 2000-01-01 07:00:00 UTC")
+    end
+
+    it "I can create a party without guests" do
+      movie = Movie.last
+      within ".party_form" do
+        fill_in :duration, with: movie.duration
+        fill_in :date, with: "12-08-2020"
+        fill_in :start_time, with: "7:00"
+        click_button "Create Viewing Party"
+      end
+    end
+
+    it "text" do
+      movie = Movie.last
+      within ".party_form" do
+        fill_in :duration, with: ""
+        fill_in :date, with: ""
+        fill_in :start_time, with: ""
+        check "#{@shaunda.name}"
+        check "#{@kiera.name}"
+        click_button "Create Viewing Party"
+      end
+      expect(page).to have_content("Date can't be blank, Start time can't be blank, and Duration is not a number")
     end
   end
 end
